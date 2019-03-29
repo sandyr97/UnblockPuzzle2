16d06ab5901d29a4dcefca544721698cc6fb9304
Alex Johnson <44007752+alex189212@users.noreply.github.com>
2019-03-29 09:47:45 -0500

    Added some clarity to the general structure of the program
    Added the setup function and the first level pattern

--- game.js
+++ game.js
@@ -1,10 +1,42 @@
 class game
 {
+  level: 1;
   constructor(){
-    this.blockArray = undefined;
+    let blockArray = [];
+    setup(blockArray); //initial call sends 1 param, second param defaults to 1
+    //a next level button can be coded in html to call a next level function that calls
+    //setup with corresponding level
   }
 }
 
+setup(blockArray, level = 1)
+{
+    switch (level) {
+      case 1:
+      levelPattern1(blockArray);
+        break;
+      case 2:
+
+        break;
+      case 3:
+
+        break;
+      case 4:
+
+        break;
+      case 5:
+
+        break;
+      default:
+
+    }
+}
+
+//each level will literally be a pattern of block and their corresponding x,y positions
+levelPattern1(blockArray)
+{
+  
+}
 
 //create the game object.
 let gameplay = new game();
