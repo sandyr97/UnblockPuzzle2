a174b1037c7a59c2deeb9d47a3c17a4c62a82c91
Cbeeb121 <beaboutclayton@gmail.com>
2019-04-08 09:47:07 -0500

    newUser.cs added

--- UNITY/BlockBloque/Assets/Scripts/MainPage.cs
+++ UNITY/BlockBloque/Assets/Scripts/MainPage.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class MainPage : MonoBehaviour
 {
-   
+    public TextAsset users;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StreamWriter writer = new StreamWriter("Assets/users.txt", true);
+        writer.WriteLine("Evan,4000");
+        writer.Close();
     }
 
     // Update is called once per frame
---
+++ UNITY/BlockBloque/Assets/Scripts/NewUser.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class StartBehaviour : MonoBehaviour 
+{
+public TextAsset users;
+public Texture buttonText;
+public InputField name, score;
+
+
+// Should run when play button is clicked
+void OnPlay()
+{
+    //Write some text to the test.txt file
+    StreamWriter writer = new StreamWriter("Assets/users.txt", true);
+    writer.WriteLine();
+    writer.Close();
+
+}
+
+}
