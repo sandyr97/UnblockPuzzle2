91b9971687681bdebe5fe61c9babd612ec2f9f12
Evan Gofourth <evangofourth99@gmail.com>
2019-04-09 14:12:42 -0500

    Added music, added file writing object.

---
+++ UNITY/BlockBloque/Assets/Scripts/WriteFile.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEditor;
+
+public class WriteFile : MonoBehaviour
+{
+    public string Path = "Assets/TextFiles/users.txt";
+    public AudioSource blockTango;
+    
+    public void WriteString(string string_to_write)
+    {
+        StreamWriter writer = new StreamWriter(Path, true);
+        writer.WriteLine(string_to_write);
+        writer.Close();
+       // AssetDatabase.ImportAsset(Path);
+       
+    }
+    public string ReadFile()
+    {
+        StreamReader reader = new StreamReader(Path);
+        return reader.ReadToEnd();
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject); //Whatever has this script will not be destroyed when levels change.
+        blockTango.Play(); //play block tango.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
