683f7ff3fe3660e7b6f9df3e8a6d58daef9d3812
Evan Gofourth <evangofourth99@gmail.com>
2019-04-30 12:04:47 -0500

    Yet Another Github Crisis Fix.

--- UNITY/BlockBloque/Assets/Scripts/Testing.cs
+++ UNITY/BlockBloque/Assets/Scripts/Testing.cs
@@ -6,9 +6,7 @@ using UnityEngine;
 public class Testing : MonoBehaviour
 {
     public Text displayText;
-    public Text displayText2;
-    public Text displayText3;
-
+  
     // Start is called before the first frame update
     void Start()
     {
@@ -20,53 +18,16 @@ public class Testing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-    public void Run()
+    public void run()
     {
-        if (Test_IsPresent_Master())
-        {
-            displayText.text = "Beginning Testing... Master object PASSED";
-        }
-        else
-        {
-            displayText.text = "Master Object Failed!";
-        }
+        
+        displayText.text = "Beginning Testing...";
+        GameObject drag = GameObject.Find("Drag");
+      
 
-        if (Test_IsPresent_BlockTango())
-        {
-            displayText2.text = "Block Tango PASSED";
-        }
-        else
-        {
-            displayText2.text = "Block Tango Failed";
-        }
 
-        if (Test_IsPresent_timer())
-        {
-            displayText3.text = "timer found. Test PASSED";
-        }
-        else
-        {
-            displayText3.text = "timer not found. Test Failed";
-        }
-
-    }
-
-    public bool Test_IsPresent_Master()
-    {
-        //Returns true if GameObject named "Master" is found, else false.
-        return GameObject.Find("Master");
-    }
-
-    public bool Test_IsPresent_BlockTango()
-    {
-        return GameObject.Find("Write&Sound").transform.Find("Audio Source");
-    }
-    public bool Test_IsPresent_timer()
-    {
-        return GameObject.Find("timer_controller");
     }
 
 }
-
